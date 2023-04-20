b7fbb5e1c817a050e0cbdd3afa6a24c33ba967ed
alux444 <aluux999@gmail.com>
2023-04-20 12:27:07 +1200

    initial prototype for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
-function App() {
+import ERROR from './img/error.png'
+import Clouds from './img/clouds.png'
+import Cloudy from './img/cloudy.png'
+import Rain from './img/rain.png'
+import Shower from './img/shower.png'
+import Snow from './img/snow.png'
+import Sunny from './img/sunny.png'
+import Thunder from './img/thunderstorm.png'
+
+const App = () => {
+
+  const [data, setData] = useState({})
+  const [city, setCity] = useState('')
+  const [error, setError] = useState(false)
+  
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_WEATHER_KEY,
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
+  };
+
+  const fetchData = () => {
+  
+  fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`, options)
+    .then(response => response.json())
+    .then(response => 
+      setData(response))
+    .then(setError(false))
+    .catch(err => setError(true))
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+    fetchData()
+    console.log(error)
+  }
+
+  const handleChange = (event) => {
+    setCity(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='container fade-animation'>
+        <form className='searchbox'
+        onSubmit={submitHandler}>
+          <input className='searchbar' 
+          type="text" 
+          value={city}
+          onChange={handleChange}
+          placeholder='Enter your city'/>
+          <button className='btn submit-btn'> Submit </button>
+      </form>
+
+      {data.current ? data.current.condition.code === 1000 ? <img src={Sunny} alt="sunny"/> : null : null}
+
+      {data.error ? <img src={ERROR} alt='error'/> : null}
+
+        <div className='weather-container'>
+          <div className='weather-box'>
+            {data.current ? (<p display='inline'>{data.current.condition.text}</p>)
+               : null}
+            {data.current ? (<p display='inline'>{data.location.name},{data.location.country}</p>)
+               : null} 
+            {data.current ? (<p display='inline'>Temperature: {data.current.temp_c}</p>)
+               : null}
+            {data.current ? (<p display='inline'>Feels like: {data.current.feelslike_c}</p>)
+               : null}
+            {data.current ? (<p display='inline'>Wind Speed: {data.current.gust_kph} kmph</p>)
+               : null} 
+            {data.current ? (<p display='inline'>Humidity: {data.current.humidity}%</p>)
+               : null} 
+            
+          </div>
+      </div>
+      </div>
+    )
 }
 
-export default App;
+export default App
